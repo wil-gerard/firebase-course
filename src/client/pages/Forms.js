f885fe84297c483d51517ee34bb1e64eaecdd997
dariatsvetkova <d.s.tsvetkova@hotmail.com>
2021-09-15 16:02:06 -0400

    refactor data card

--- src/client/pages/Forms.js
+++ src/client/pages/Forms.js
@@ -10,6 +10,7 @@ import {
 import firebase from '../../firebase/clientApp';
 
 import Form from '../components/Form';
+import DataCard from '../components/DataCard';
 
 const Forms = () => {
   // Fetching data from Firestore
@@ -53,64 +54,11 @@ const Forms = () => {
       </Helmet>
 
       <main>
-        <div className="bg-white shadow rounded-lg w-11/12 sm:w-6/12 mx-auto my-4 z-10">
-          <div className="px-4 py-5 sm:p-6">
-            {loading && (
-              <>
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
-                  Just a sec
-                </h3>
-                <div className="mt-2 max-w-xl text-sm text-gray-500">
-                  <p>Loading data from Firebase...</p>
-                </div>
-              </>
-            )}
-            {error && (
-              <>
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
-                  Oops
-                </h3>
-                <div className="mt-2 max-w-xl text-sm text-gray-500">
-                  <p>Can&apos;t fetch the data. Open the console to see details</p>
-                </div>
-              </>
-            )}
-            {value && value.docs.length > 0 && (
-              <>
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
-                  Your favourite animal is:
-                </h3>
-                <div className="mt-2 max-w-xl text-sm text-gray-500">
-                  {value.docs.map((doc) => (
-                    <p key={doc.data().id}>{doc.data().animal}</p>
-                  ))}
-                  {value.docs.length > 1 && (
-                    <div className="rounded-md bg-yellow-50 p-4">
-                      <h3 className="text-sm font-medium text-yellow-800">This looks suspicious</h3>
-                      <div className="mt-2 text-sm text-yellow-700">
-                        <p>
-                          If you see more than one animal here,
-                          document creation did not go as planned
-                        </p>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              </>
-            )}
-            {value && !value.docs.length && (
-              <>
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
-                  Enter the data below
-                </h3>
-                <div className="mt-2 max-w-xl text-sm text-gray-500">
-                  <p>to save it to Firestore and see it appear here</p>
-                </div>
-              </>
-            )}
-          </div>
-        </div>
-
+        <DataCard
+          value={value}
+          loading={loading}
+          error={error}
+        />
         <Form
           handleData={handleData}
           text={{
