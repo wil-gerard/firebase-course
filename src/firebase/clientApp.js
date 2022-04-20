b59e2736c18ca7537a7f2382666db1f599235795
dariatsvetkova <d.s.tsvetkova@hotmail.com>
2022-04-20 14:09:03 -0400

    use demo project

--- src/firebase/clientApp.js
+++ src/firebase/clientApp.js
@@ -6,13 +6,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 // Replace this with your app's configuration from Firebase Console
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  appId: process.env.FIREBASE_APP_ID,
-};
+const firebaseConfig = { projectId: 'demo-codebusters' };
 
 firebase.initializeApp(firebaseConfig);
 
